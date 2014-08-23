e2c8b7751996927c4a9f99d780f9cb2a1928b6b4
Austin Hansen <austinthansen@gmail.com>
2014-08-23 15:30:50 -0600

    logic for generating grids

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -3,4 +3,50 @@ class Pattern < ActiveRecord::Base
   validates_presence_of :minor_y
   validates_presence_of :major_x
   validates_presence_of :major_y
+  before_save :create_grid
+
+  private
+
+  def create_grid
+    @grid = ""
+    generate_lines
+    self.grid = @grid.chop
+  end
+
+  def generate_lines
+    i = 0
+    while i < major_y
+      minor_y.times do
+        add_line_to_grid(i)
+      end
+      i += 1
+    end
+  end
+
+  def add_line_to_grid(i)
+    if i.odd?
+      @grid << create_line + "\n"
+    else
+      @grid << create_line.reverse + "\n"
+    end
+  end
+
+  def create_line
+    i = 0
+    @unit = ""
+    while i < major_x
+      add_unit(i)
+      i += 1
+    end
+    @unit
+  end
+
+  def add_unit(i)
+    if i.even?
+      character = "X"
+    else
+      character = "O"
+    end
+    @unit += (character * minor_x)
+  end
 end
--- spec/models/pattern_spec.rb
+++ spec/models/pattern_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 describe Pattern, :type => :model do
+  it { should validate_presence_of(:minor_x) }
+  it { should validate_presence_of(:minor_y) }
+  it { should validate_presence_of(:major_x) }
+  it { should validate_presence_of(:major_y) }
+
   it 'should have a valid factory' do
     expect(build :pattern).to be_valid
+  end
+
+  it 'should generate a grid string from x and y values' do
+    pattern = create(:pattern, minor_x: 4, major_x: 3, minor_y: 4, major_y: 5)
+    expect(pattern.grid).to be_a(String)
   end
 end
