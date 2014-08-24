90a5a764d0304ea854f9b38436efa6b43a5f0972
Austin Hansen <austinthansen@gmail.com>
2014-08-23 21:54:02 -0600

    Pattern uses grid_generator to find grid value

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,3 +1,5 @@
+require 'grid_generator'
+
 class Pattern < ActiveRecord::Base
   validates_presence_of :minor_x
   validates_presence_of :minor_y
@@ -8,49 +10,7 @@ class Pattern < ActiveRecord::Base
   private
 
   def create_grid
-    @grid = []
-    generate_lines
-    self.grid = @grid.each {|x| puts x}
-  end
-
-  def generate_lines
-    @row = 0
-    while @row < major_y
-      minor_y.times do
-        add_line_to_grid
-      end
-      @row += 1
-    end
-  end
-
-  def add_line_to_grid
-    if @row.even?
-      @grid << create_line
-    else
-      @grid << create_line.reverse
-    end
-  end
-
-  def create_line
-    i = 0
-    @unit = ""
-    while i < major_x
-      add_unit(i)
-      i += 1
-    end
-    @unit
-  end
-
-  def add_unit(i)
-    if i.even? && @row.odd? && major_x.odd?
-      character = "O"
-    elsif i.odd? && @row.odd? && major_x.odd?
-      character = "X"
-    elsif i.even?
-      character = "X"
-    else
-      character = "O"
-    end
-    @unit += (character * minor_x)
+    generator = GridGenerator.new(self.minor_x, self.major_x, self.minor_y, self.major_y)
+    self.grid = generator.grid
   end
 end
